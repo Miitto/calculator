2cb2c53d139d7f342c4af518a8cde5b1e0451523
ElPadre-OD <sarim1989@gmail.com>
2023-08-14 00:39:40 +0100

    finding opr idx

--- calculator/calc/views.py
+++ calculator/calc/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
 
 def index(request):
-  return render(request, "index.html")
+  if request.method == "GET":
+    return render(request, "index.html")
+  elif request.method == "POST":
+    eqStr = request.POST.get("eq")
+    eqStr = "".join(eqStr.split())
+    opr = "*/+-"
+    idx = eqStr.find("*")
+    while idx != -1:
+      lowbound = max([eqStr.find(s, 0, idx) for s in opr])
+      highbound = min([eqStr.find(s, idx) for s in opr])
+      
+  
